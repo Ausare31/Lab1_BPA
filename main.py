10d8f197adc30a250458dcb819ac0711c2fbdcc7
Ausare31 <89301505+Ausare31@users.noreply.github.com>
2023-02-21 21:47:36 +0600

    add file

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import requests
-from bs4 import BeautifulSoup
-url = 'https://omgtu.ru/ecab/persons/index.php?b=1'
-page = requests.get(url)
-page_parsed = BeautifulSoup(page.text, 'html.parser')
-employees = page_parsed.findAll('div', style="padding: 5px; font-size: 120%;")
-with open('result.txt', 'w') as f:
-    for employee in employees:
-        name = employee.find('a').text.strip()
-        f.write(name + '\n')
+import requests # импортируем библиотеку requests для выполнения http-запросов
+from bs4 import BeautifulSoup # импортируем библиотеку BeautifulSoup для разбора html-кода
+def parse():
+    url = 'https://omgtu.ru/ecab/persons/index.php?b=1' # URL адрес
+    page = requests.get(url) # методом get отправляем запрос на сервер и получаем ответ в переменную
+    print(page.status_code)  # выводим ответ сервера, чтобы убедиться, что запрос был успешным
+    page_parsed = BeautifulSoup(page.text, 'html.parser') # обрабатываем page, передав в качестве аргументов html-код страницы и парсер, который будет использоваться для его обработки.
+    employees = page_parsed.findAll('div', style="padding: 5px; font-size: 120%;") #  находим на странице все элементы div, у которых заданы стили padding: 5px и font-size: 120%, и сохраняем их
+    with open('result.txt', 'w') as f: # открываем, а точнее создаем файл в режиме записи
+        for employee in employees: # проходимся в цикле по всем элементам списка employees
+            name = employee.find('a').text.strip() # находим в каждом элементе div первый тег 'a', получаем текст из нее методом text, удаляем лишние пробелы в начале и конце строки методом strip и сохраняем имя в переменную name.
+            f.write(name + '\n') # записываем имя сотрудника в файл
